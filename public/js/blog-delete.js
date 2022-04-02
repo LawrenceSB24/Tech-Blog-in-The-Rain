149a6a4ba475f7321ff271da55b5a49c2d88119c
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-02 13:41:16 -0400

    JS files for handling functionality

---
+++ public/js/blog-delete.js
@@ -0,0 +1,22 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (comment_text) {
+        const response = await fetch(`/api/blogs/${id}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.delete-blog-btn').addEventListener('click', deleteFormHandler);
---
+++ public/js/blog-edit.js
@@ -0,0 +1,30 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name = "blog-title"]').value.trim();
+    const comment = document.querySelector('input[name = "blog-comment"]').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (comment_text) {
+        const response = await fetch(`/api/blogs/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                comment,
+                description: comment
+            }),
+
+            headers: {'Content-Type': 'application/json'}
+        });
+
+        if (response.ok) {
+            document.location.replace();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
