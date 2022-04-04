f21c917395ccd4c051ee90c1568aaa0be948f327
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-03 22:21:09 -0400

    Fixed issues with seed data

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -3,17 +3,17 @@ const { Blog } = require('../models');
 const blogData = [
     {
         title: "Test Blog 1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         user_id: 1
     },
     {
         title: "Test Blog 2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         user_id:2
     },
     {
         title: "Test Blog 3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         user_id: 3
     }
 
--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,19 +2,19 @@ const {Comments} = require('../models');
 
 const comData = [
     {
-        description: 'I can always rely on ORMs',
+        comment_description: 'I can always rely on ORMs',
         user_id: 1,
         blog_id: 1
     },
 
     {
-        description: 'Thank you for explaining the MVC paradigm structure',
+        comment_description: 'Thank you for explaining the MVC paradigm structure',
         user_id: 2,
         blog_id: 2
     },
 
     {
-        description: 'Thank you for explaining handlebars.js in clearer details',
+        comment_description: 'Thank you for explaining handlebars.js in clearer details',
         user_id: 3,
         blog_id: 3
     }
