a45c8f9fe344950dc1815e94ccb98b6e932064fe
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-03 22:19:51 -0400

    Fixed signup functionality for new users

--- public/js/login.js
+++ public/js/login.js
@@ -22,13 +22,14 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#username-login').ariaValueMax.trim();
-    const password = document.querySelector('#password-login').ariaValueMax.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
+    if (username && email && password) {
+        const response = await fetch('/api/users/signup', {
             method: 'POST',
-            body: JSON.stringify({username, password}),
+            body: JSON.stringify({username, email, password}),
             headers: {'Content-Type': 'application/json'}
         });
 
